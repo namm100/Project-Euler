178cf36ebac6d85fab4848ce5d8967cf46e9cf77
namm100 <namm0@icloud.com>
2016-08-12 16:00:05 -0400

    added brute force problem25

--- problem25.cpp
+++ problem25.cpp
@@ -1,17 +1,29 @@
 #include <stdio.h>
 #include "BigInteger.h"
 #include <string>
-
+#include <sstream>
+/*
+What is the index of the first term in the fibonacci
+sequence to contain 1000 digits?
+*/
 using namespace std;
 
-
+void endDig(string stop) {
+	printf("{\n");
+	string currDig = "0";
+	string count = "1";
+	while (!(currDig.compare(stop) == 0)) {
+		string currF = BigInteger::fibonacciStr(count);
+		int currF_int = (int) currF.length();
+		ostringstream ss;
+		ss << currF_int;
+		currDig = ss.str();
+		printf("%s: %s: %s\n", count.c_str(), currF.c_str(),currDig.c_str());
+		count = BigInteger::addStr(count, "1");
+	}
+	printf("}\n");	
+}
 int main(int argc, char** argv) {
-	
-	string a = "7";
-	string fb = "0";
-	string res = BigInteger::powStr(a,fb);
-	
-	printf("%s - %s = %s\n", a.c_str(), fb.c_str(), res.c_str());
-	
+	endDig("1000");	
 	return 0;
 }
